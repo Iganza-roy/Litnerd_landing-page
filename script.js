c273bf2c0b6f14ed5693e1c8c627ab1afab2a5c4
Iganza-roy <iganzaroy55@gmail.com>
2024-07-08 22:44:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 function navigateToApp() {
-  window.location.href = "https://your-deployed-app-url.com";
+  window.location.href =
+    "https://lit-nerd-letters-8sk2nrhfh-roy-iganza-kibtatas-projects.vercel.app/";
 }
